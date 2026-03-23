6f0c76a32f6030a121d13cad02e659d762294cf0
jayram833 <jaypansare96@gmail.com>
2026-03-23 21:12:37 +0530

    fix: adds separation in two task items

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -2,7 +2,7 @@ import TaskCard from "./TaskCard";
 
 export default function Tasks({ data, col }) {
     const { tasks, columns } = data;
-    return <div>
+    return <div className="task-list">
         {columns[col].map((item) => {
             return <TaskCard key={tasks[item].id} task={tasks[item]} />
         })}
